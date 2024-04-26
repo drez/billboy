d7b40cfd25f591dd4053b9e08d4ded725c827127
Fred <fred@apigoat.com>
2024-04-26 10:01:50 +0800

    latest

--- .admin/config/routes.php
+++ .admin/config/routes.php
@@ -1,13 +1,26 @@
 <?php
 
+namespace App;
+
+use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use ApiGoat\Routes\RouteHelper;
+
+
 /**
  * Routes definitions
  */
 
-use Slim\App;
 
 return function (App $app) {
     include 'Built/routes.php';
 
     /* declare new routes here */
+    /*$app->post(_SUB_DIR_URL . 'RefreshStats', function (Request $request, Response $response, $args) {
+        $RouteHelper = new RouteHelper($request, $args);
+        $Service = new \App\StatsService($request, $response, $RouteHelper->getArgs());
+        return $Service->getResponse();
+    })->setName('RefreshStats');*/
+
 };
